b2231ebfa7049686dd339aafc7284c546fb1f61c
airtonxjunior <antonioarj1@puccampinas.edu.br>
2025-07-19 18:38:25 -0300

    atualização hospedagem

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from flask import Flask
 from routes.user import user_route
+from database.database import criar_conexao # Importa a função de conexão para teste inicial
 
-#cria a aplicação Flask.
-app = Flask('__name__')
+# Cria a aplicação Flask.
+app = Flask(__name__)
 
 #chave secreta para sessões
 app.secret_key = '9f8K!2@#v9sa7%$8vjsn3p9!sZ'
 
-#registra a blueprint (rotas em user_route) na aplicação.
 app.register_blueprint(user_route)
 
-#inicia o servidor
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
+
+try:
+    conn = criar_conexao()
+    if conn:
+        print("Conexão com o banco de dados estabelecida com sucesso na inicialização do app.")
+        conn.close()
+    else:
+        print("Falha ao estabelecer conexão com o banco de dados na inicialização do app.")
+except Exception as e:
+    print(f"Erro ao tentar conectar ao banco de dados na inicialização do app: {e}")
 
